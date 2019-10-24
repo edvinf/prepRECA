dea82849528c47cc1fe5e30ae57b97f0e7a6bd04
edvinf <edvin.fuglebakk@gmail.com>
2019-10-24 19:21:58 +0200

    Data update

--- R/samples-datadoc.R
+++ R/samples-datadoc.R
@@ -1,12 +1,13 @@
 #' Data from Norwegian port sampling program.
 #'
-#' Norwegian port sampling for landings in 2018 of fresh whitefish north of Lat 64, covering Jig, Gillnet, Longline, and Demershal Seining
+#' Norwegian port sampling for landings in 2018 of fresh whitefish north of Lat 64, covering Jig, Gillnet, Longline, and Demershal Seining.
 #' Sampling is implemented by a research vessel systematically sampling ports accepting fresh fish along the northern Norwegian coast.
 #' Landings are almost exclusively day-catches located to small areas.
 #' Vessels are intercepted during landing, and samples of up to 30 fish are taken stratified by gear and species.
 #' Length, weight and age measured for all fish. Sex and maturity when presentation allows for it.
-#' Weight of fish is reported as measured (encoded in SApres), while total weight of landing and sample is converted to Whole-weight using constant factors standardized by the Norwegian Directorate of Fisheries.
-#' \href{https://www.fiskeridir.no/Yrkesfiske/Tall-og-analyse/Omregningsfaktorer}{Conversion factors (in Norwegian)}.
+#' Weight of individual fish is reported as measured (presentation encoded in SApres), while total weight of landing and sample is converted to Whole-weight
+#' using constant factors standardized by the Norwegian Directorate of Fisheries (See table 2.1 in \href{https://www.fiskeridir.no/Yrkesfiske/Tall-og-analyse/Omregningsfaktorer}{Conversion factors}. Document is mostly in Norwegian, but the relevant table is in English as well).
+#'
 #'
 #' @docType data
 #'
